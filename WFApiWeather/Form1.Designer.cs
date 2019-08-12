890d95b3965945202a91a73782d45b7c27f0bc71
Грушко Станислав Валерьевич <s.grushko@bankvostok.com.ua>
2019-08-12 10:01:55 +0300

    Add project files.

---
+++ WFApiWeather/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WFApiWeather
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.lbl_city = new System.Windows.Forms.Label();
+            this.textBox_city = new System.Windows.Forms.TextBox();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tmin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tmax = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pressure = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Humidity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Clouds = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Weather = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Wind = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Date,
+            this.Tmin,
+            this.Tmax,
+            this.Pressure,
+            this.Humidity,
+            this.Clouds,
+            this.Weather,
+            this.Wind});
+            this.dgv.Location = new System.Drawing.Point(12, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(954, 389);
+            this.dgv.TabIndex = 0;
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(876, 407);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(90, 31);
+            this.btn_update.TabIndex = 1;
+            this.btn_update.Text = "Update Data";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.Btn_update_Click);
+            // 
+            // lbl_city
+            // 
+            this.lbl_city.AutoSize = true;
+            this.lbl_city.Location = new System.Drawing.Point(519, 416);
+            this.lbl_city.Name = "lbl_city";
+            this.lbl_city.Size = new System.Drawing.Size(105, 13);
+            this.lbl_city.TabIndex = 2;
+            this.lbl_city.Text = "Населённый пункт:";
+            // 
+            // textBox_city
+            // 
+            this.textBox_city.Location = new System.Drawing.Point(640, 413);
+            this.textBox_city.Name = "textBox_city";
+            this.textBox_city.ReadOnly = true;
+            this.textBox_city.Size = new System.Drawing.Size(213, 20);
+            this.textBox_city.TabIndex = 3;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            this.Date.Width = 150;
+            // 
+            // Tmin
+            // 
+            this.Tmin.HeaderText = "Tmin, °C";
+            this.Tmin.Name = "Tmin";
+            // 
+            // Tmax
+            // 
+            this.Tmax.HeaderText = "Tmax,°C";
+            this.Tmax.Name = "Tmax";
+            // 
+            // Pressure
+            // 
+            this.Pressure.HeaderText = "Pressure, kPa";
+            this.Pressure.Name = "Pressure";
+            // 
+            // Humidity
+            // 
+            this.Humidity.HeaderText = "Humidity, %";
+            this.Humidity.Name = "Humidity";
+            // 
+            // Clouds
+            // 
+            this.Clouds.HeaderText = "Clouds, %";
+            this.Clouds.Name = "Clouds";
+            // 
+            // Weather
+            // 
+            this.Weather.HeaderText = "Weather";
+            this.Weather.Name = "Weather";
+            // 
+            // Wind
+            // 
+            this.Wind.HeaderText = "Wind, m/sec";
+            this.Wind.Name = "Wind";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 450);
+            this.Controls.Add(this.textBox_city);
+            this.Controls.Add(this.lbl_city);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.dgv);
+            this.Name = "MainForm";
+            this.Text = "Open Weather Map";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Label lbl_city;
+        private System.Windows.Forms.TextBox textBox_city;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tmin;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tmax;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pressure;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Humidity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Clouds;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Weather;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Wind;
+    }
+}
+
---
+++ WFApiWeather/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFApiWeather
+{
+    public partial class MainForm : Form
+    {
+        public static string lastError = "";
+        public static string weatherData = "";
+        object[] obj;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_update_Click(object sender, EventArgs e)
+        {
+            dgv.Rows.Clear();
+            textBox_city.Text = "";
+
+            GetWeatherData();
+            if (lastError != "")
+            {
+                MessageBox.Show(lastError, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (weatherData != "")
+                {
+                    WeatherRoot currentWeather = JsonConvert.DeserializeObject<WeatherRoot>(weatherData);
+                    MessageBox.Show("Данные загружены!", "Инфо", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    textBox_city.Text = currentWeather.city.country + ", " + currentWeather.city.name;
+
+
+                    foreach (List list in currentWeather.list)
+                    {
+                        obj = new object[] {
+                            list.dt_txt,
+                            list.main.temp_min,
+                            list.main.temp_max,
+                            list.main.pressure,
+                            list.main.humidity,
+                            list.clouds.all,
+                            list.weather[0].main,
+                            "" + Math.Round(list.wind.deg) + "°; " + Math.Round(list.wind.speed)
+                        };
+                        dgv.Rows.Add(obj);
+                        obj = null;
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Отсутствуют данные о погоде", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private static void GetWeatherData()
+        {
+
+            lastError = "";
+            WebClient client = new WebClient();
+            try
+            {
+                using (Stream stream = client.OpenRead("https://api.openweathermap.org/data/2.5/forecast?q=Odessa,ua&appid=dac392b2d2745b3adf08ca26054d78c4&lang=ru&units=metric"))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        weatherData = reader.ReadToEnd();
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                lastError = e.Message;
+            }
+        }
+
+
+    }
+}
